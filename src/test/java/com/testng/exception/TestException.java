d5a969925646785ea80e6425f5696ba334c48e47
RushikeshRajeshKaralkar <karalkarrushikesh21@gmail.com>
2020-04-27 12:10:05 +0530

    This is second Qa Commit

---
+++ src/test/java/com/testng/exception/TestException.java
@@ -0,0 +1,19 @@
+package com.testng.exception;
+
+import org.testng.annotations.Test;
+
+public class TestException {
+
+	@Test
+	public void Test1() {
+		System.out.println("This is Test1 Of ");
+
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("This is Test2 Of ");
+
+	}
+
+}
